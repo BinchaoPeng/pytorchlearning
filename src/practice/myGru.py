8782342397e1726024d6121d687eeb4b33102c3c
ubuntupbc <646047990@qq.com>
2021-03-04 10:11:48 +0800

    practice-gru

---
+++ src/practice/myGru.py
@@ -0,0 +1,31 @@
+import torch
+import torch.nn as nn
+
+batch_size = 3
+seq_lens = 4
+input_size = 2
+hidden_size = 5
+num_layers = 1
+bidirectional = True
+
+n_direction = 2 if bidirectional else 1
+
+gru = nn.GRU(input_size=input_size, hidden_size=hidden_size, num_layers=num_layers, bidirectional=bidirectional)
+
+input = torch.randn(seq_lens, batch_size, input_size)
+hidden = torch.zeros(num_layers * n_direction, batch_size, hidden_size)
+
+out, hn = gru(input, hidden)
+
+print(gru.parameters())
+
+print("input:", input)
+print("input shape:", input.shape)
+
+print("hidden:", hidden)
+print("hidden shape:", hidden.shape)
+
+print("out:", out)
+print("out shape:", out.shape)
+print("hn:", hn)
+print("hn shape:", hn.shape)
